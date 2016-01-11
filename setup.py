c392f7faed5b3fba688376f9bbe773b70c0219ef
Darko Aleksovski <darko.aleksovski@ijs.si>
2016-01-11 12:49:12 +0100

    Package renamed

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='cf_noise',
+    name='cf_noise_detection',
     version='0.1',
     include_package_data=True,
     license='MIT License',
-    description='Package which includes the widgets from noise and perfeval categories, and other related widgets.',
+    description='A ClowdFlows package containing noise detection widgets. The package is intended to be used along with ClowdFlows 2.0.',
     
     # install_requires=[
     #       'scikit-learn>=0.14.1',
